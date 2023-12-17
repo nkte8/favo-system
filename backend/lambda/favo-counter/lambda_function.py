dcf8e0f2ee51823643253a6fd2b4428125fa874d
nekono <91360587+nkte8@users.noreply.github.com>
2023-12-17 12:08:23 +0000

    シンプルに動作するAPIの作成完了

--- backend/lambda/favo-counter/lambda_function.py
+++ backend/lambda/favo-counter/lambda_function.py
@@ -1,3 +1,56 @@
+import json
+from favo_system import fav_api
+
+favo_table = "favodb"
+user_table = "userdb"
+
+urlquery_identify = "name"
+# local_endpoint_url = "http://localhost:4566"
+
+def err(s_code):
+    msg = ""
+    match s_code:
+        case 500:
+            msg = "Internal Server Error."
+        case 400:
+            msg = "Bad Request."
+    return {
+        'statusCode': s_code,
+        'body': msg
+    }
+
 def handler(event, context):
-    print(event)
-    return 'hello world?'
+    result = {}
+    method = event['requestContext']['http']['method']  
+    if method != "POST" and method != "GET":
+        return err(500)
+
+    identify = event['queryStringParameters'].get(urlquery_identify)
+
+    if identify == None:
+        return err(400)
+
+    # page_url = json.loads(identify)
+    api = fav_api(
+        favo_table)
+
+    match method:
+        case "GET":
+            result = api.page_fav_read(identify)
+        case "POST":
+            result = api.page_fav_add(identify)
+
+    return result
+
+if __name__ == "__main__":
+    event = {
+        "requestContext": {
+            "http": {
+                "method": "GET"
+            }
+        },
+        "queryStringParameters": {
+            urlquery_identify: "/hogehoge"
+        }
+    }
+    handler(event=event, context="")
--- backend/terraform/lambda.tf
+++ backend/terraform/lambda.tf
@@ -1,37 +1,61 @@
 # lambda.tf
 
-# data "archive_file" "lambda_favo_counter" {
-#   type        = "zip"
-#   source_dir = "lambda/favo-counter/"
-#   output_path = "lambda_function_payload.zip"
-# }
+variable "lambda_favo_system_payload_name" {}
+variable "lambda_favo_system_payload_src" {}
+variable "lambda_favo_system_name" {}
+variable "lambda_favo_system_handler" {}
 
-# resource "aws_lambda_function" "lambda_function_favo_counter" {
-#   filename      = "lambda_function_payload.zip"
-#   function_name = "lambda_function_name"
-#   role          = aws_iam_role.iam_for_lambda.arn
-#   handler       = "lambda_function.handler"
+variable "lambda_layer_payload_name" {}
+variable "lambda_layer_payload_src" {}
+variable "lambda_layer_name" {}
 
-#   source_code_hash = data.archive_file.lambda_favo_counter.output_base64sha256
+data "archive_file" "lambda_favo_system_payload" {
+  type        = "zip"
+  source_dir = var.lambda_favo_system_payload_src
+  output_path = var.lambda_favo_system_payload_name
+}
 
-#   runtime = "python3.11"
+resource "aws_lambda_function" "lambda_favo_system_function" {
+  filename      = var.lambda_favo_system_payload_name
+  function_name = var.lambda_favo_system_name
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = var.lambda_favo_system_handler
 
-#   environment {
-#     variables = {
-#       foo = "bar"
-#     }
-#   }
-# }
+  source_code_hash = data.archive_file.lambda_favo_system_payload.output_base64sha256
 
-# data "archive_file" "lambda_layer_archive" {
-#   type        = "zip"
-#   source_dir = "lambda/layers/"
-#   output_path = "lambda_layer_payload.zip"
-# }
+  runtime = "python3.11"
+}
 
-# resource "aws_lambda_layer_version" "lambda_layer" {
-#   filename   = "lambda_layer_payload.zip"
-#   layer_name = "lambda_layer_name"
+data "archive_file" "lambda_layer_archive" {
+  type        = "zip"
+  source_dir = var.lambda_layer_payload_src
+  output_path = var.lambda_layer_payload_name
+}
 
-#   compatible_runtimes = ["python3.11"]
-# }
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename   = var.lambda_layer_payload_name
+  layer_name = var.lambda_layer_name
+
+  compatible_runtimes = ["python3.11"]
+}
+
+resource "aws_lambda_function_url" "lambda_favo_system_function_url" {
+  function_name      = aws_lambda_function.lambda_favo_system_function.arn
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials  = true
+    allow_origins      = ["*"]
+    allow_methods      = ["POST","GET"]
+    allow_headers      = ["date", "keep-alive"]
+    expose_headers     = ["keep-alive", "date"]
+    max_age            = 86400
+  }
+  depends_on = [
+    aws_lambda_function.lambda_favo_system_function
+  ]
+}
+
+output "lambda_favo_system_function_url" {
+  value = aws_lambda_function_url.lambda_favo_system_function_url.function_url
+}
