4a3f34f6e813a56cee9030a29cd386af26931aa5
nkte8 <mail@nekono.dev>
2023-12-23 15:30:53 +0000

    フロントエンドの開発実施、API仕様を変更

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Favobutton from './components/favobutton/main'
 
+const api_url = "http://enxjokg82n65ifweyvy33u88z0f4c43g.lambda-url.us-east-1.localhost.localstack.cloud:4566/"
+const page_name = "hoge"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Favobutton api_url={api_url} page_name={page_name} />
       </header>
     </div>
   );
