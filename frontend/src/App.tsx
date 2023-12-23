59c6301f2af3f19042c7a81f417df1fed662b14f
nekono <91360587+nkte8@users.noreply.github.com>
2023-12-23 15:42:12 +0000

    register工程を削除

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import Favobutton from './components/favobutton/main'
 
-const api_url = "http://enxjokg82n65ifweyvy33u88z0f4c43g.lambda-url.us-east-1.localhost.localstack.cloud:4566/"
+const api_url = "http://sr341tl8ofh2j6j13yeso7rvmacnk2nh.lambda-url.us-east-1.localhost.localstack.cloud:4566/"
 const page_name = "hoge"
 function App() {
   return (
