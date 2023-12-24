892456fb952149d5e27994a0ef56ab925d689b36
nkte8 <nkte8@outlook.jp>
2023-12-24 14:43:50 +0000

    userdbにも作用するようにコードを追加/修正

--- backend/terraform/lambda.tf
+++ backend/terraform/lambda.tf
@@ -1,27 +1,32 @@
 # lambda.tf
 
-variable "lambda_favo_system_payload_name" {}
-variable "lambda_favo_system_payload_src" {}
-variable "lambda_favo_system_name" {}
-variable "lambda_favo_system_handler" {}
+variable "lambda_favo_counter_payload_name" {}
+variable "lambda_favo_counter_payload_src" {}
+variable "lambda_favo_counter_name" {}
+variable "lambda_favo_counter_handler" {}
+
+# variable "lambda_ufav_counter_payload_name" {}
+# variable "lambda_ufav_counter_payload_src" {}
+# variable "lambda_ufav_counter_name" {}
+# variable "lambda_ufav_counter_handler" {}
 
 variable "lambda_layer_payload_name" {}
 variable "lambda_layer_payload_src" {}
 variable "lambda_layer_name" {}
 
-data "archive_file" "lambda_favo_system_payload" {
+data "archive_file" "lambda_favo_counter_payload" {
   type        = "zip"
-  source_dir = var.lambda_favo_system_payload_src
-  output_path = var.lambda_favo_system_payload_name
+  source_dir = var.lambda_favo_counter_payload_src
+  output_path = var.lambda_favo_counter_payload_name
 }
 
-resource "aws_lambda_function" "lambda_favo_system_function" {
-  filename      = var.lambda_favo_system_payload_name
-  function_name = var.lambda_favo_system_name
+resource "aws_lambda_function" "lambda_favo_counter_function" {
+  filename      = var.lambda_favo_counter_payload_name
+  function_name = var.lambda_favo_counter_name
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = var.lambda_favo_system_handler
+  handler       = var.lambda_favo_counter_handler
 
-  source_code_hash = data.archive_file.lambda_favo_system_payload.output_base64sha256
+  source_code_hash = data.archive_file.lambda_favo_counter_payload.output_base64sha256
 
   runtime = "python3.11"
 }
@@ -39,8 +44,8 @@ resource "aws_lambda_layer_version" "lambda_layer" {
   compatible_runtimes = ["python3.11"]
 }
 
-resource "aws_lambda_function_url" "lambda_favo_system_function_url" {
-  function_name      = aws_lambda_function.lambda_favo_system_function.arn
+resource "aws_lambda_function_url" "lambda_favo_counter_function_url" {
+  function_name      = aws_lambda_function.lambda_favo_counter_function.arn
   authorization_type = "NONE"
 
   cors {
@@ -52,10 +57,10 @@ resource "aws_lambda_function_url" "lambda_favo_system_function_url" {
     max_age            = 86400
   }
   depends_on = [
-    aws_lambda_function.lambda_favo_system_function
+    aws_lambda_function.lambda_favo_counter_function
   ]
 }
 
-output "lambda_favo_system_function_url" {
-  value = aws_lambda_function_url.lambda_favo_system_function_url.function_url
+output "lambda_favo_counter_function_url" {
+  value = aws_lambda_function_url.lambda_favo_counter_function_url.function_url
 }
