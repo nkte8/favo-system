118238f3ddeaac39fd160073d8f5c120a1c67065
nkte8 <mail@nekono.dev>
2023-12-27 11:54:03 +0000

    ユーザ登録コンポーネントの改修

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,21 @@ import './App.css';
 import Favobutton from './components/favobutton/main'
 import Dropbutton from './components/dropbutton/main'
 import Registeruser from './components/registeruser/main'
+import Userlabel from './components/userlabel/main'
 
 const api_url = "http://lsgh7xzkuz2dqul69u7lcr6hcgdtij9b.lambda-url.us-east-1.localhost.localstack.cloud:4566/"
 const page_name = "hoge"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Registeruser />
-        <Favobutton api_url={api_url} page_name={page_name} />
-        <Dropbutton api_url={api_url} />
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div>Welcome <Userlabel /></div>
+				<Registeruser />
+				<Favobutton api_url={api_url} page_name={page_name} />
+				<Dropbutton api_url={api_url} />
+			</header>
+		</div>
+	);
 }
 
 export default App;
