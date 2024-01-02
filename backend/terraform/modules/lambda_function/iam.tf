89f256047734165e63c1b36fc97c8f22c62243e3
nkte8 <nkte8@outlook.jp>
2024-01-02 19:13:05 +0000

    local環境/prod環境を明確に分割するための構造改修

--- backend/terraform/modules/lambda_function/iam.tf
+++ backend/terraform/modules/lambda_function/iam.tf
@@ -14,16 +14,11 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "dynamodb_access" {
-  role = aws_iam_role.iam_for_lambda.id
+  role = aws_iam_role.iam_lambda.id
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
-# resource "aws_iam_role_policy_attachment" "cloudwatch_log_access" {
-#   role = aws_iam_role.iam_for_lambda.id
-#   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-# }
-
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
+resource "aws_iam_role" "iam_lambda" {
+  name               = var.iam_lambda
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
---
+++ backend/terraform/prod/provider.tf
@@ -0,0 +1,21 @@
+# provider.tf
+variable "aws_region" {}
+
+provider "aws" {
+	region     = var.aws_region
+}
+
+terraform {
+	required_providers {
+		aws = {
+			source  = "hashicorp/aws"
+			version = "~> 5.0"
+		}
+	}
+	backend "s3" {
+		bucket  = "favo-system"
+		region  = "ap-northeast-1"
+		key     = "terraform.tfstate"
+		encrypt = true
+	}
+}
